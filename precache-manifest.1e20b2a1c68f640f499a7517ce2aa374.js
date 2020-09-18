dcbe416bb00441b8b020b90967c2453da96277d0
Sfundo Mhlungu <mhlungusk@gmail.com>
2020-09-18 02:05:34 +0200

    Updates

---
+++ precache-manifest.1e20b2a1c68f640f499a7517ce2aa374.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d600f0372c4f33f742efa0cb28befd",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "29b3f277dd049ce6c682",
+    "url": "/Portfolio/static/css/main.de09027b.chunk.css"
+  },
+  {
+    "revision": "2be7f9607c0bcf48b139",
+    "url": "/Portfolio/static/js/2.cfbe7937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.cfbe7937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b3f277dd049ce6c682",
+    "url": "/Portfolio/static/js/main.62be0660.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "7b8dfa6fce652adc5eddfaf377b0e219",
+    "url": "/Portfolio/static/media/Admin.7b8dfa6f.png"
+  },
+  {
+    "revision": "1ff1c47d5dc019e6ae6274cd1c97749c",
+    "url": "/Portfolio/static/media/Calculatir.1ff1c47d.png"
+  },
+  {
+    "revision": "10fc3bee17aab335ce4998ba07ded005",
+    "url": "/Portfolio/static/media/Dope bot.10fc3bee.jpg"
+  },
+  {
+    "revision": "af15301854998fb020a72f957b3cf8e5",
+    "url": "/Portfolio/static/media/Drum Machine.af153018.png"
+  },
+  {
+    "revision": "699582d4cf25b675de89046358e4e113",
+    "url": "/Portfolio/static/media/Exitium.699582d4.png"
+  },
+  {
+    "revision": "1e94b863f77537c205cde683d6ec80e6",
+    "url": "/Portfolio/static/media/Markdown actual code.1e94b863.png"
+  },
+  {
+    "revision": "6fc21e9cd7f3ccc449ab6f79a2c9a011",
+    "url": "/Portfolio/static/media/Markdown preview.6fc21e9c.png"
+  },
+  {
+    "revision": "90378a104f6f2c521337a37c4feac317",
+    "url": "/Portfolio/static/media/Pomodoro clock.90378a10.png"
+  },
+  {
+    "revision": "22cdf7d9e8853daf5e6b33ee3f710d5e",
+    "url": "/Portfolio/static/media/Random vs MinMax.22cdf7d9.png"
+  },
+  {
+    "revision": "38829975076b60ff0191109e6e935b0e",
+    "url": "/Portfolio/static/media/bayesian-2889576_640.38829975.png"
+  },
+  {
+    "revision": "e005eb7898e40cadd756f40432cb4c28",
+    "url": "/Portfolio/static/media/business-1839876_1920.e005eb78.jpg"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/Portfolio/static/media/github (2).b6ec19ce.png"
+  },
+  {
+    "revision": "d803d7f7ee93eec7f3ed0f96f14bfdc4",
+    "url": "/Portfolio/static/media/me siting.d803d7f7.jpg"
+  },
+  {
+    "revision": "9ac8e3c20f4a04479796c2bd23e9533f",
+    "url": "/Portfolio/static/media/sfundoDevimg.9ac8e3c2.jpg"
+  }
+]);
